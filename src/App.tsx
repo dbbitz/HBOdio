81434a02bb7a24634dcad8f6329a9f41411cd91c
Daniel Barbosa Rocha <81662636+dbbitz@users.noreply.github.com>
2022-11-21 11:25:29 -0300

    feat: estrutura de pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +10,6 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
