60adc29d76a0bfb2cec09f7ef2dae0d090d990d8
Daniel Barbosa Rocha <81662636+dbbitz@users.noreply.github.com>
2022-11-23 14:04:10 -0300

    fix

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,13 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Catalog } from "../pages/Catalog";
+import { Error404 } from "../pages/Error404";
 
 export const Router = () => (
     <Routes>
+        <Route path="*" element={<Error404/>}/>
+        <Route path="/" element={<Navigate to="/home"/>} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/catalog" element={<Catalog />} />
