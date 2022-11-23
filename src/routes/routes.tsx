ea58f8bd60254c7de12376ace5a6bf9d4b59a655
Daniel Barbosa Rocha <81662636+dbbitz@users.noreply.github.com>
2022-11-23 08:57:35 -0300

    done: routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
+import { Catalog } from "../pages/Catalog";
+
+export const Route = () => (
+    0
+);
